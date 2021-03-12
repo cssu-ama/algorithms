fbabc5d45ee99dd2df157a88f53fc62d8e46f847
amir <cssu.ama@gmail.com>
2021-03-12 21:44:29 +0330

    Update Main.java

--- CombinigMergeSortAndInsertionSort/Main.java
+++ CombinigMergeSortAndInsertionSort/Main.java
@@ -1,13 +1,13 @@
 public class Main {
   public static void main(String args[]) {
-      int array[] = { 12, 11, 13, 5, 6, 7 };
+    int array[] = { 12, 11, 13, 5, 6, 7 };
   
-      System.out.println("Given Array");
-      Sort.printArray(array);
+    System.out.println("Given Array");
+    Sort.printArray(array);
   
-      Sort.sort(array);
+    Sort.sort(array);
 
-      System.out.println("Sorted array");
-      Sort.printArray(array);
+    System.out.println("Sorted array");
+    Sort.printArray(array);
   }
 }
