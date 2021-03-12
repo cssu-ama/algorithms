d9548d6805d4189899d6e49b735fff47be148164
cssu-ama <cssu.ama@gmail.com>
2021-03-12 17:05:24 +0330

    Adding Inversion count in merge sort algorithm

---
+++ MergeSortWithInversionCount/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+		int[] array = { 1, 20, 6, 4, 5 };
+
+		System.out.println("Inversion count: " + MergeSort.countInversion(array));
+	}
+}
