97abfc057ec921aa3acc480b4be1f184b0ac7b77
amir <cssu.ama@gmail.com>
2021-03-12 21:48:31 +0330

    Update MergeSort.java

--- CombinigMergeSortAndInsertionSort/MergeSort.java
+++ CombinigMergeSortAndInsertionSort/MergeSort.java
@@ -1,43 +1,44 @@
 public class MergeSort {
   public static void merge(int array[], int left, int middle, int right) {
-		int leftArraySize = middle - left + 1;
-		int rightArraySize = right - middle;
+    int leftArraySize = middle - left + 1;
+    int rightArraySize = right - middle;
 
-		int leftArray[] = new int[leftArraySize];
-		int rightArray[] = new int[rightArraySize];
+    int leftArray[] = new int[leftArraySize];
+    int rightArray[] = new int[rightArraySize];
 
-		for (int i = 0; i < leftArraySize; ++i) {
-			leftArray[i] = array[left + i];
+    for (int i = 0; i < leftArraySize; ++i) {
+      leftArray[i] = array[left + i];
     }
 
-		for (int j = 0; j < rightArraySize; ++j) {
-			rightArray[j] = array[middle + 1 + j];
+    for (int j = 0; j < rightArraySize; ++j) {
+      rightArray[j] = array[middle + 1 + j];
     }
 
-		int i = 0, j = 0;
+    int i = 0, j = 0;
 
-		int k = left;
-		while (i < leftArraySize && j < rightArraySize) {
-			if (leftArray[i] <= rightArray[j]) {
-				array[k] = leftArray[i];
-				i++;
-			} else {
-				array[k] = rightArray[j];
-				j++;
-			}
-			k++;
-		}
+    int k = left;
+    while (i < leftArraySize && j < rightArraySize) {
+      if (leftArray[i] <= rightArray[j]) {
+        array[k] = leftArray[i];
+        i++;
+      } else {
+        array[k] = rightArray[j];
+        j++;
+      }
 
-		while (i < leftArraySize) {
-			array[k] = leftArray[i];
-			i++;
-			k++;
-		}
+      k++;
+    }
 
-		while (j < rightArraySize) {
-			array[k] = rightArray[j];
-			j++;
-			k++;
-		}
-	}
+    while (i < leftArraySize) {
+      array[k] = leftArray[i];
+      i++;
+      k++;
+    }
+
+    while (j < rightArraySize) {
+      array[k] = rightArray[j];
+      j++;
+      k++;
+    }
+  }
 }
