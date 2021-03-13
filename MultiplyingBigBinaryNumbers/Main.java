352a2a7584a5d51b7b215b28f54c3522df79d1fe
amir <cssu.ama@gmail.com>
2021-03-13 19:10:57 +0330

    Spacing each line correctly

--- MultiplyingBigBinaryNumbers/Main.java
+++ MultiplyingBigBinaryNumbers/Main.java
@@ -1,7 +1,7 @@
 public class Main {
-	public static void main(String[] args) {
-		System.out.println(Karatsuba.multiply(200, 200));
-		System.out.println(Karatsuba.multiply(12345, 6789));
-		System.out.println(Karatsuba.multiply(2358925, 1259174));
-	}
+  public static void main(String[] args) {
+    System.out.println(Karatsuba.multiply(200, 200));
+    System.out.println(Karatsuba.multiply(12345, 6789));
+    System.out.println(Karatsuba.multiply(2358925, 1259174));
+  }
 }
