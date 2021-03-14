0816585d05409b2243d49906ef0b5e2c5155f522
amir <cssu.ama@gmail.com>
2021-03-14 12:47:43 +0330

    Update InsertionSort.java

--- RecursiveInsertionSort/InsertionSort.java
+++ RecursiveInsertionSort/InsertionSort.java
@@ -3,19 +3,19 @@ public class InsertionSort {
     sort(array, array.length);
   }
 
-	private static void sort(int array[], int n) { 
-		if (n <= 1) {	return; }
+  private static void sort(int array[], int n) { 
+    if (n <= 1) { return; }
 
-		sort(array, n - 1); 
+    sort(array, n - 1); 
 
-		int last = array[n - 1]; 
-		int j = n - 2; 
+    int last = array[n - 1]; 
+    int j = n - 2; 
 
-		while (j >= 0 && array[j] > last) { 
-			array[j + 1] = array[j]; 
-			j--; 
-		}
+    while (j >= 0 && array[j] > last) { 
+      array[j + 1] = array[j]; 
+      j--; 
+    }
 
-		array[j + 1] = last; 
-	}
+    array[j + 1] = last; 
+  }
 } 
